1cbecc850f6a66b08faabb5e309c69c5c00dc83f
Imam Hossain Santho <imamhosssain@gmail.com>
2020-05-27 19:38:19 +0600

    added weather app skeleton

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,62 @@
+const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
-// app.com
-// app.com/help
-// app.com/about
+// Define paths for Express config 
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.render('index', {
+        title: 'Weather App',
+        details: 'The app can show current and daily forecast',
+        author: 'Imam Hossain'
+    })
+})
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        author: 'Imam Hossain'
+    })
+})
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Section',
+        query: 'Your Questions Goes Here',
+        author: 'Imam Hossain'
+    })
+})
+app.get('/weather', (req, res) => {
+    res.send({
+        forcast: 20,
+        location: 'Dhaka Bangladesh'
+    })
+})
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        message: 'Help article not found!',
+        author: 'Imam Hossain'
+    })
+})
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        message: 'Page not found!',
+        author: 'Imam Hossain'
+    })
 })
 app.listen(9000, ()=>{
-    console.log('Server is up on port 3005.')
+    console.log('Server is up on port 9000.')
 })
