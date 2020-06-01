51ba13c7e6ee6249b54336bed6ce3b45da5bac84
Imam Hossain Santho <imamhosssain@gmail.com>
2020-06-02 02:01:29 +0600

    cleaned repository

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,43 +1,57 @@
-const weatherForm = document.querySelector("#weather-form-1");
-const search = document.querySelector("#search-text-1");
+//input and output selector
+const weatherForm = document.querySelector("#weather-form-1")
+const search = document.querySelector("#search-text-1")
 
-const timeFormat = { hour: "2-digit", minute: "2-digit" };
-const dateFormat = { year: "numeric", month: "long", day: "numeric" };
-let time = new Date().toLocaleTimeString("en-US", timeFormat);
-let date = new Date().toLocaleDateString("en-US", dateFormat);
-let timeSelector = document.querySelector(".time");
+//current time and date function
+const timeFormat = { hour: "2-digit", minute: "2-digit" }
+const dateFormat = { year: "numeric", month: "long", day: "numeric" }
+let time = new Date().toLocaleTimeString("en-US", timeFormat)
+let date = new Date().toLocaleDateString("en-US", dateFormat)
+
+//Selector
+let timeSelector = document.querySelector(".time")
+let locationBar = document.querySelector(".location-bar")
+let main = document.querySelector(".weather")
+
+
 timeSelector.innerHTML = `<p>Time: ${time}</p>
-                         <p>Date: ${date}</p>`;
+                          <p>Date: ${date}</p>`
+main.innerHTML = ""
 
+//event listener
 weatherForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+  e.preventDefault()
 
-  let location = search.value;
-  let locationBar = document.querySelector(".location-bar");
-  locationBar.innerHTML = `<p>Loading: please wait.......</p>`;
-  //console.log(location);
+    let location = search.value
+    let place = location.split(",")[0]
+  
+ locationBar.innerHTML = `<p>Loading: please wait.......</p>`
 
+  main.innerHTML = ""
   fetch(`/weather?address=${location}`)
     .then((response) => response.json())
     .then((data) => {
       if (data.error) {
-        let main = document.querySelector(".weather");
+        
         timeSelector.innerHTML = `<p>Time: ${time}</p>
-                         <p>Date: ${date}</p>`;
-        locationBar.innerHTML = `</p>Location: Location not found!</p>`;
-        main.innerHTML = `<div class="error">
+                         <p>Date: ${date}</p>`
+        locationBar.innerHTML = `</p>Location: Location not found!</p>`
+        main.innerHTML =  `<div class="error">
                                   <p>${data.error}</p>
-                                  <p> Please try again! </p> </div>`;
+                                  <p> Please try again! </p> </div>`
       } else {
-        let imageUrl = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png`;
-        let place = data.location.split(",")[0];
-        timeSelector.innerHTML = `<p>Time: ${data.time}</p>
-                         <p>Date: ${data.date}</p>`;
-        let location = `<p>Location: ${data.location}</p>
-                                <p>Latitude: ${data.latitude}</p>
-                                <p>Longitude: ${data.longitude}</p>`;
-        locationBar.innerHTML = location;
+          let { time, date, location, latitude, longitude } = data
+          let { condition, forecast, sunset, sunrise, current, daily } = data
 
+        let imageUrl = `http://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png`
+          
+          
+        timeSelector.innerHTML = `<p>Time: ${time}</p>
+                         <p>Date: ${date}</p>`
+        locationBar.innerHTML = `<p>Location: ${location}</p>
+                        <p>Latitude: ${latitude}</p>
+                        <p>Longitude: ${longitude}</p>`
+          
         let html = ` <section class="current">
                 <section class="current-weather">
                     <div class="current-location">
@@ -50,17 +64,17 @@ weatherForm.addEventListener("submit", (e) => {
                     </div>
 
                     <div class="current-condition">
-                        <div class="temp">${data.temp}\u2103</div>
-                        <div class="summary">${data.condition}</div>
+                        <div class="temp">${current.temp}\u2103</div>
+                        <div class="summary">${condition}</div>
                     </div>
                 </section>
                 <section class="current-info">
                     <ul class="info-list">
-                        <li>Feels Like: ${data.current.feels_like}\u2103</li>
-                        <li>humidity: ${data.current.humidity}%</li>
-                        <li>wind: ${data.current.wind_speed}m/s</li>
-                        <li>cloud cover: ${data.current.clouds}%</li>
-                        <li>visibility: ${data.current.visibility / 1000}km</li>
+                        <li>Feels Like: ${current.feels_like}\u2103</li>
+                        <li>humidity: ${current.humidity}%</li>
+                        <li>wind: ${current.wind_speed}m/s</li>
+                        <li>cloud cover: ${current.clouds}%</li>
+                        <li>visibility: ${current.visibility / 1000}km</li>
                     
                     </ul>
                 </section>
@@ -68,16 +82,16 @@ weatherForm.addEventListener("submit", (e) => {
             <section class="summary">
                 <div class="forecast-summary">
                     <h2>Next 24 hours:</h2>
-                    <p>&nbsp;${data.forecast}</p>
+                    <p>&nbsp;${forecast}</p>
                 </div>
                 <div class="other-info">
-                    <p>Sunrise: ${data.sunrise}</p>
-                    <p>Sunset: ${data.sunset}</p>
-                    <p>Day Max: ${data.daily[0].temp.max}\u2103</p>
-                    <p>Day Min: ${data.daily[0].temp.min}\u2103</p>
+                    <p>Sunrise: ${sunrise}</p>
+                    <p>Sunset: ${sunset}</p>
+                    <p>Day Max: ${daily[0].temp.max}\u2103</p>
+                    <p>Day Min: ${daily[0].temp.min}\u2103</p>
                 </div>
-            </section>`;
-        document.querySelector(".weather").innerHTML = html;
+            </section>`
+        main.innerHTML = html
       }
-    });
-});
+    })
+})
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,104 +1,97 @@
-const path = require("path");
-const express = require("express");
-const hbs = require("hbs");
-const geocode = require("./utils/geocode");
-const weather = require("./utils/weather");
-require("./utils/polyfills").default;
-const app = express();
+const path = require("path")
+const express = require("express")
+const hbs = require("hbs")
+const geocode = require("./utils/geocode")
+const weather = require("./utils/weather")
+require("./utils/polyfills").default
+const app = express()
 const port = process.env.PORT || 9000
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials");
+const publicDirectoryPath = path.join(__dirname, "../public")
+const viewsPath = path.join(__dirname, "../templates/views")
+const partialsPath = path.join(__dirname, "../templates/partials")
 
 // Setup handlebars engine and views location
-app.set("view engine", "hbs");
-app.set("views", viewsPath);
-hbs.registerPartials(partialsPath);
+app.set("view engine", "hbs")
+app.set("views", viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath));
+app.use(express.static(publicDirectoryPath))
 
 app.get("", (req, res) => {
   res.render("index", {
     title: "Weather App",
     details: "The app can show current and daily forecast",
     author: "Imam Hossain",
-  });
-});
+  })
+})
 app.get("/about", (req, res) => {
   res.render("about", {
     title: "About Me",
     author: "Imam Hossain",
-  });
-});
+  })
+})
 app.get("/help", (req, res) => {
   res.render("help", {
     title: "Help Section",
     query: "Your Questions Goes Here",
     author: "Imam Hossain",
-  });
-});
+  })
+})
 app.get("/weather/?*", (req, res) => {
   if (!req.query.address) {
-    return res.send({ error: "Address is not provided" });
+    return res.send({ error: "Address is not provided" })
   }
   geocode(req.query.address, function geoCodeCallback(error, response) {
     if (error) {
-      return res.send({ error });
+      return res.send({ error })
     }
     //console.log(data)
-    let { latitude, longitude, location } = response;
+    let { latitude, longitude, location } = response
 
     weather(latitude, longitude, function weatherCallback(error, response) {
       if (error) {
-        return res.send({ error });
+        return res.send({ error })
       }
 
-      let { body: weatherData } = response;
+      let { body: weatherData } = response
 
-      let timezone = weatherData.timezone;
+      let { current, daily, timezone } = weatherData
+      
       let myObject = {
         latitude,
         longitude,
         time: new Date().formatTime(timezone),
         date: new Date().formatDate(timezone),
-        updateTime: new Date(weatherData.current.dt * 1000).formatTime(
-          timezone
-        ),
+        updateTime: new Date(current.dt * 1000).formatTime(timezone),
         location,
-        sunrise: new Date(weatherData.current.sunrise * 1000).formatTime(
-          timezone
-        ),
-        sunset: new Date(weatherData.current.sunset * 1000).formatTime(
-          timezone
-        ),
-        temp: weatherData.current.temp,
-        feelsLike: weatherData.current.feels_like,
-        condition: weatherData.current.weather[0].description.toTitleCase(),
-        forecast: weatherData.daily[0].weather[0].description.toTitleCase(),
-      };
-      let result = { ...weatherData, ...myObject };
+        sunrise: new Date(current.sunrise * 1000).formatTime(timezone),
+        sunset: new Date(current.sunset * 1000).formatTime(timezone),
+        condition: current.weather[0].description.toTitleCase(),
+        forecast: daily[0].weather[0].description.toTitleCase(),
+      }
+      let result = { ...weatherData, ...myObject }
 
-      res.send(result);
-    });
-  });
-});
+      res.send(result)
+    })
+  })
+})
 app.get("/help/*", (req, res) => {
   res.render("404", {
     title: "404",
     message: "Help article not found!",
     author: "Imam Hossain",
-  });
-});
+  })
+})
 app.get("*", (req, res) => {
   res.render("404", {
     title: "404",
     message: "Page not found!",
     author: "Imam Hossain",
-  });
-});
+  })
+})
 
 app.listen(port, () => {
-  console.log("Server is up on port " + port);
-});
+  console.log("Server is up on port " + port)
+})
